3048c86617dfa906ae5f7ab33ae6b4c83f825414
DevilxHacker <166166887+DevilxHacker@users.noreply.github.com>
2025-09-28 23:35:23 +0530

    Configured Server Upload

--- backend/src/services/pdfService.js
+++ backend/src/services/pdfService.js
@@ -84,6 +84,7 @@ export async function markdownToPDFBuffer(markdownText) {
     // Puppeteer → PDF
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "domcontentloaded" });
